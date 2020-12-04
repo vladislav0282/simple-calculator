f3244c7bb1c2853474732b6221882ef0350470bd
vladislav0282 <ubpsv31460@yandex.ru>
2020-12-04 19:29:12 +0300

    add first task

---
+++ lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int a, b;
+            Console.WriteLine("Введите число 1");
+            str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+            Console.WriteLine("Введите число 2");
+            str = Console.ReadLine();
+            b = Convert.ToInt32(str);
+            int result = a + b;
+            Console.WriteLine("Сумма чисел = " + result);
+
+
+
+        }
+    }
+}
